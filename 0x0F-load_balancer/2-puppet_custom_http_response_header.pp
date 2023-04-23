a547ba5358eb5ae6f5786908b7d5f4b6e65ae9ec
Evance Odoyo <odoyoevanc@gmail.com>
2023-04-23 23:17:37 +0300

    puppet custom http response header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# Add a custom HTTP header with Puppet
+exec { 'http header':
+	command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default
+	sudo service nginx restart',
+	provider => shell,
+}
