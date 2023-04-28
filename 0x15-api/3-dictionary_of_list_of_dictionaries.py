8dd5f2f5d04718d2317174640c5cbd702e985c6f
Evance Odoyo <odoyoevanc@gmail.com>
2023-04-29 01:11:51 +0300

    Dictionary of lists of dictionaries: export all employee data to JSON

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script to export data in JSON format.
+Usage: ./3-dictionary_of_list_of_dictionaries.py
+"""
+
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/"
+    employees = requests.get(url).json()
+    
+    emp_data = {}
+    for emp in employees:
+        emp_id = str(emp.get('id'))
+        emp_data[emp_id] = []
+
+        emp_tasks = requests.get(url + emp_id + "/todos").json()
+        for task in emp_tasks:
+            task_data = {}
+            task_data['username'] = emp.get('username')
+            task_data['task'] = task.get('title')
+            task_data['completed'] = task.get('completed')
+            emp_data[emp_id].append(task_data)
+
+    with open("todo_all_employees.json", "w") as f:
+         json.dump(emp_data, f)
